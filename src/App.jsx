6478118bacdf8fc6d54bc43da34a8f190dbc69f7
Victor <victor.r.oliveira@aluno.senai.br>
2026-08-18 09:45:07 -0300

    atualizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
-import Sidebar from "./components/Sidebar";
+import Sidebar from "./components/sidebar";
 import RotaPrivada from "./components/RotaPrivada";
 import Dashboard from "./pages/dashboard";
 import Login from "./pages/login.jsx";
