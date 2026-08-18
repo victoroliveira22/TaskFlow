24fd6bde74406028adfa294f82cf20c66a228675
Victor <victor.r.oliveira@aluno.senai.br>
2026-08-18 09:44:09 -0300

    atualizado

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
 import RotaPrivada from "./components/RotaPrivada";
-import Dashboard from "./pages/Dashboard";
-import Login from "./pages/Login.jsx";
-import Sobre from "./pages/Sobre.jsx";
+import Dashboard from "./pages/dashboard";
+import Login from "./pages/login.jsx";
+import Sobre from "./pages/sobre.jsx";
 
 function App() {
   const [tarefas, setTarefas] = useState(() => {
--- src/pages/sobre.jsx
+++ src/pages/sobre.jsx
@@ -1,4 +1,4 @@
-import "./Sobre.css";
+import "./sobre.css";
 import React from 'react';
 function Sobre() {
   return (
